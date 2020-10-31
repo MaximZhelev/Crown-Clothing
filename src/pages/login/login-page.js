3fdba674a83e36a3856c67a48b0c9f771a723a1d
MaximZhelev <maxim.jelev@gmail.com>
2020-10-31 18:35:35 +0200

    Included ability to store authenticated users into firestore database.

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 import "./login-page.scss";
 
 const LoginPage = () => (
   <div className="login">
     <SignIn />
+    <SignUp />
   </div>
 );
 
