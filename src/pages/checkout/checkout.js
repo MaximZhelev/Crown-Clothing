a4785993a8222841e9d3f4a83827724f42e9ccbb
MaximZhelev <maxim.jelev@gmail.com>
2020-11-02 18:27:36 +0200

    Added stripe integration.

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item";
-
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button";
 import {
   selectCartItems,
   selectCartTotal,
@@ -40,6 +40,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
