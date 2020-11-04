bf746a422768ca836427bdb09be2e2dbbc2cf908
MaximZhelev <maxim.jelev@gmail.com>
2020-11-04 21:09:26 +0200

    Added redux thunk

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+import shop from "../../pages/shop/shop";
 
 const selectShop = (state) => state.shop;
 
@@ -17,3 +18,12 @@ export const selectCollection = (collectionUrlParam) =>
   createSelector([selectCollections], (collections) =>
     collections ? collections[collectionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
+export const selectIsCollectionLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections //We are using double bang for shorthand Boolean type casting.
+)
